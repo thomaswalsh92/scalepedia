0e638a248525bde62bcd17d389d39942b7c790e2
thomaswalsh92 <thomaseawalsh@gmail.com>
2021-08-22 22:30:48 +0200

    enabled passing of props from App.js to Widget.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 
 import './fonts.css'
 import './App.scss';
+import React, { useState } from 'react';
 import { NavBar } from './NavBar/NavBar.js'
 import { Hero } from './Hero/Hero.js'
 import { Widget } from './Widget/Widget';
 
 
 function App() {
+
+  const [rootNote, setRootNote ] = React.useState('C')
+  const [scaleType, setScaleType ] = React.useState('Major')
+
+  function changeRootNote(newRootNote) {
+    setRootNote(newRootNote);
+  }
+
+  function changeScaleType (newScaleType) {
+    setScaleType(newScaleType);
+  }
+
   return (
     <div className='app'>
     <NavBar />
     <Hero />
       <div className='main'>
-      <Widget />
+      <Widget rootNote={rootNote} changeRootNote={changeRootNote} scaleType={scaleType} changeScaleType={changeScaleType} />
       </div>
     </div>
   );
--- src/Widget/Widget.js
+++ src/Widget/Widget.js
@@ -1,12 +1,11 @@
 import { ControlBar } from './ControlBar.js'
 import { NoteSection } from './NoteSection.js'
 
-export const Widget = () => {
+export const Widget = (props) => {
     return (
         <div className='widget'>
             <ControlBar />
-            <p>Keyboard section</p>
-            <NoteSection />
+            <p>This is the widget section and root note is: {props.rootNote}, the scale type is {props.scaleType}.</p>
         </div>
     )
 
