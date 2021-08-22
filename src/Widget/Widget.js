ed544cf52042ca6333ddbea2e19b86a951d3dfa4
thomaswalsh92 <thomaseawalsh@gmail.com>
2021-08-22 21:39:46 +0200

    added note section funcion with dummy data

--- src/Widget/Widget.js
+++ src/Widget/Widget.js
@@ -1,11 +1,12 @@
 import { controlBar } from './controlBar.js'
+import { noteSection } from './noteSection.js'
 
 export const widget = () => {
     return (
         <div className='widget'>
             {controlBar()}
             <p>Keyboard section</p>
-            <p>Note section</p>
+            {noteSection()}
         </div>
     )
 
---
+++ src/Widget/noteSection.js
@@ -0,0 +1,26 @@
+export const noteSection = () => {
+    return (
+        <div className='noteSection'>
+            <table>
+                <tr>
+                    <th>Root</th>
+                    <th>2nd</th>
+                    <th>3rd</th>
+                    <th>4th</th>
+                    <th>5th</th>
+                    <th>6th</th>
+                    <th>7th</th>
+                </tr>
+                <tr>
+                    <td>C</td>
+                    <td>D</td>
+                    <td>E</td>
+                    <td>F</td>
+                    <td>G</td>
+                    <td>A</td>
+                    <td>B</td>
+                </tr>
+            </table>
+        </div>
+      );
+};
